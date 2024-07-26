0b0f38218cc8388041c380d74eb83b812c9e0d29
0hxin <qkrdowls1890@naver.com>
2024-07-26 10:45:38 +0900

    예약 정보 db 저장 완ㄹ

---
+++ src/main/java/bitc/fullstack/sleepon/repository/UserReservationRepository.java
@@ -0,0 +1,7 @@
+package bitc.fullstack.sleepon.repository;
+
+import bitc.fullstack.sleepon.model.UserReservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReservationRepository extends JpaRepository<UserReservation, Long> {
+}
